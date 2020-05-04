42ebfac83300aa60e4097e761b7b54c21212d33c
Andrey <stig1467@gmail.com>
2020-05-04 18:52:55 +0300

    added transition to signup screen

--- ChatApp/Controller/Auth/LoginController.swift
+++ ChatApp/Controller/Auth/LoginController.swift
@@ -33,8 +33,9 @@ class LoginController: UIViewController {
         let button = UIButton(type: .system)
         button.setTitle("Log in", for: .normal)
         button.layer.cornerRadius = 5
-        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        button.backgroundColor = .red
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        button.setTitleColor(.white, for: .normal)
         button.setHeight(height: 50)
         return button
     }()
@@ -47,10 +48,29 @@ class LoginController: UIViewController {
        return tf
     }()
     
+    private let dontHaveaccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account? ", attributes: [.font: UIFont.systemFont(ofSize: 16), .foregroundColor: UIColor.white])
+        
+        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [.font: UIFont.boldSystemFont(ofSize: 16), .foregroundColor: UIColor.white]))
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureUI()
+    }
+    
+    @objc func handleShowSignUp() {
+        let controller = RegController()
+        navigationController?.pushViewController(controller, animated: true)
     }
     
     func configureUI() {
@@ -75,6 +95,9 @@ class LoginController: UIViewController {
         stack.spacing = 16
         view.addSubview(stack)
         stack.anchor(top: iconImage.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
+        
+        view.addSubview(dontHaveaccountButton)
+        dontHaveaccountButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingLeft: 32, paddingRight: 32)
     }
     
     func configureGradientLayer() {
