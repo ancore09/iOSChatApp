f7c5e42d07bbe5f0175044e0465fe8ef6f9ac882
Andrey <stig1467@gmail.com>
2020-05-04 19:15:18 +0300

    registration screen

--- ChatApp/Controller/Auth/LoginController.swift
+++ ChatApp/Controller/Auth/LoginController.swift
@@ -99,12 +99,4 @@ class LoginController: UIViewController {
         view.addSubview(dontHaveaccountButton)
         dontHaveaccountButton.anchor(left: view.leftAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingLeft: 32, paddingRight: 32)
     }
-    
-    func configureGradientLayer() {
-        let gradient = CAGradientLayer()
-        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemPink.cgColor]
-        gradient.locations = [0, 1]
-        view.layer.addSublayer(gradient)
-        gradient.frame = view.frame
-    }
 }
--- ChatApp/Controller/Auth/RegController.swift
+++ ChatApp/Controller/Auth/RegController.swift
@@ -10,8 +10,82 @@ import UIKit
 
 class RegController: UIViewController {
     
+    private lazy var emailContainer: UIView = {
+        let containerView = InputContainerView(image: #imageLiteral(resourceName: "ic_mail_outline_white_2x"), textField: emailTextField)
+        
+        return containerView
+    }()
+    
+    private lazy var fullNameContainer: UIView = {
+        let containerView = InputContainerView(image: #imageLiteral(resourceName: "ic_person_outline_white_2x"), textField: fullNameTextField)
+        
+        return containerView
+    }()
+    
+    private lazy var userNameContainer: UIView = {
+        let containerView = InputContainerView(image: #imageLiteral(resourceName: "ic_person_outline_white_2x"), textField: userNameTextField)
+        
+        return containerView
+    }()
+    
+    private lazy var passwordContainer: InputContainerView = {
+        let containerView = InputContainerView(image: #imageLiteral(resourceName: "ic_lock_outline_white_2x"), textField: passwordTextField)
+        
+        return containerView
+    }()
+    
+    private let userNameTextField = CustomTextField(placeholder: "Username")
+    
+    private let fullNameTextField = CustomTextField(placeholder: "Full Name")
+    
+    private let emailTextField = CustomTextField(placeholder: "Email")
+    
+    private let passwordTextField: CustomTextField = {
+       let tf = CustomTextField(placeholder: "Password")
+        tf.isSecureTextEntry = true
+       return tf
+    }()
+    
+    private let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+        button.backgroundColor = #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)
+        button.setTitleColor(.white, for: .normal)
+        button.setHeight(height: 50)
+        return button
+    }()
+    
+    private let plusPhotoButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "plus_photo"), for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(handleSelectPhoto), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        configureUI()
+    }
+    
+    @objc func handleSelectPhoto() {
+        
+    }
+    
+    func configureUI() {
+        configureGradientLayer()
+        
+        view.addSubview(plusPhotoButton)
+        plusPhotoButton.centerX(inView: view)
+        plusPhotoButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        plusPhotoButton.setDimensions(height: 200, width: 200)
+        
+        let stack = UIStackView(arrangedSubviews: [emailContainer, passwordContainer, fullNameContainer, userNameContainer, signUpButton])
+        stack.axis = .vertical
+        stack.spacing = 16
+        view.addSubview(stack)
+        stack.anchor(top: plusPhotoButton.bottomAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 32, paddingLeft: 32, paddingRight: 32)
     }
 }
